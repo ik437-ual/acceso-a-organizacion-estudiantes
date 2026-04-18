e798fc9d5be00dbd7425f4cf4827acfac82af298
ik437-ual <ik437@inlumine.ual.es>
2026-04-18 18:18:26 +0000

    yo

---
+++ PR1/aceleracionPR.py
@@ -0,0 +1,9 @@
+# Leer datos por teclado
+omega = float(input())
+R = float(input())
+
+# Calcular aceleración
+aceleracion = omega ** 2 * R
+
+# Mostrar resultado
+print(aceleracion)
---
+++ PR1/enordenPR.py
@@ -0,0 +1,10 @@
+# Leer los tres números
+a = float(input())
+b = float(input())
+c = float(input())
+
+# Comprobar si están en orden creciente
+en_orden = a <= b <= c
+
+# Mostrar resultado lógico
+print(en_orden)
